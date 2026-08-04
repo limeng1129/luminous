f4a32c3faa557e3360cc1ead5e045af76a301b32
limeng1129 <85001824+limeng1129@users.noreply.github.com>
2026-08-04 17:38:46 +0800

    Add files via upload
     add fat-loss tracker

---
+++ static/fit-sw.js
@@ -0,0 +1,26 @@
+// 减脂追踪 · 离线缓存
+const CACHE = 'fit-227031a8';
+const FILES = ['./fit.html', './fit.webmanifest',
+               './fit-icon-180.png', './fit-icon-192.png', './fit-icon-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).catch(() => {}));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./fit.html')))
+  );
+});
